a6921212693bb849ed042bfc8fc6c52c362938b2
tiago-daniel <tiagosantosescola@gmail.com>
2025-02-25 18:18:13 +0000

    python scripts

---
+++ assign1/src/matrixproduct.py
@@ -0,0 +1,51 @@
+import sys
+import time
+
+def matrixCalc(col,matrix1, matrix2):
+    matrix3 = []
+    for i in range(col):
+        for j in range(col):
+            temp = 0
+            for k in range(col):
+                temp += matrix1[i*col+k] * matrix2[k*col+j]
+            matrix3.append(temp)
+    return matrix3
+
+def matrixLine(col,matrix1,matrix2):
+    matrix3 = [0] * (col * col)
+    for i in range(col):
+        for k in range(col):
+            temp = matrix1[i*col+k]
+            for j in range(col):
+                matrix3[i*col+j] += temp*matrix2[k*col+j]
+    return matrix3
+
+def main(op, col):
+    if op != 1 and op != 2:
+        print("Bad Input, Wrong Operation, please use 1 or 2")
+        return 
+    
+    matrix1 = []
+    matrix2 = []
+
+    for i in range(col):    
+        for j in range(col):
+            matrix1.append(float(1.0))
+
+    for i in range(col):    
+        for j in range(col):
+            matrix2.append(float(i+1.0))
+
+    start = time.time()
+    if op == 1:
+        matrix = matrixCalc(col,matrix1,matrix2)
+    else:
+        matrix = matrixLine(col,matrix1,matrix2)
+    end = time.time()
+    print(f"Time elapsed in seconds : {end-start}")
+    print(f"First value of matrix : {matrix[0]}")
+    
+if __name__ == "__main__":
+    op = int(sys.argv[1])
+    col = int(sys.argv[2])
+    main(op,col)
---
+++ assign1/src/matrixproduct2d.py
@@ -0,0 +1,58 @@
+import sys
+import time
+
+def matrixCalc(col,matrix1, matrix2):
+    matrixTemp = []
+    for i in range(col):
+        matrix3 = []
+        for j in range(col):
+            temp = 0
+            for k in range(col):
+                temp += matrix1[i][k] * matrix2[k][j]
+            matrixTemp.append(temp)
+        matrix3.append(matrixTemp)
+    return matrix3
+
+def matrixLine(col,matrix1,matrix2):
+    matrix3 = [[0] * col] * col
+    for i in range(col):
+        matrixTemp = []
+        for k in range(col):
+            temp = matrix1[i][k]
+            for j in range(col):
+                matrix3[i][j] += temp*matrix2[k][j]
+        return matrix3
+
+def main(op, col):
+    if op != 1 and op != 2:
+        print("Bad Input, Wrong Operation, please use 1 or 2")
+        return 
+    
+    matrix1 = []
+    matrix2 = []
+
+    for i in range(col): 
+        temp = []   
+        for j in range(col):
+            temp.append(float(1.0))
+        matrix1.append(temp)
+
+    for i in range(col):    
+        temp = []
+        for j in range(col):
+            temp.append(float(i+1.0))
+        matrix2.append(temp)
+
+    start = time.time()
+    if op == 1:
+        matrix = matrixCalc(col,matrix1,matrix2)
+    else:
+        matrix = matrixLine(col,matrix1,matrix2)
+    end = time.time()
+    print(f"First value of matrix : {matrix}")
+    print(f"Time elapsed in seconds : {end-start}")
+    
+if __name__ == "__main__":
+    op = int(sys.argv[1])
+    col = int(sys.argv[2])
+    main(op,col)
