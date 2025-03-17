f0c058843907ea40a448e7539ba2da3dc626aab3
tiago-daniel <tiagosantosescola@gmail.com>
2025-03-17 00:16:50 +0000

    parallel1, parallel2

--- assign1/src/matrixproduct.cpp
+++ assign1/src/matrixproduct.cpp
@@ -130,7 +130,7 @@ void OnMultLine(int m_ar, int m_br)
 }
 
 // add code here for line x line matriz multiplication
-void OnMultLineParallel(int m_ar, int m_br)
+void OnMultLineParallel1(int m_ar, int m_br)
 {
 	SYSTEMTIME Time1, Time2;
 
@@ -187,7 +187,64 @@ void OnMultLineParallel(int m_ar, int m_br)
 	free(phb);
 	free(phc);
 }
+void OnMultLineParallel2(int m_ar, int m_br)
+{
+	SYSTEMTIME Time1, Time2;
 
+	char st[100];
+	double temp;
+	int i, j, k;
+
+	double *pha, *phb, *phc;
+
+	pha = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phb = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phc = (double *)malloc((m_ar * m_ar) * sizeof(double));
+
+	for(i=0; i<m_ar; i++)
+		for(j=0; j<m_ar; j++)
+			pha[i*m_ar + j] = (double)1.0;
+
+	for(i=0; i<m_br; i++)
+		for(j=0; j<m_br; j++)
+			phb[i*m_br + j] = (double)(i+1);
+
+	// we need to initialize the matrix C
+	for(i=0; i<m_ar; i++)
+		for(j=0; j<m_ar; j++)
+			phc[i*m_ar + j] = (double)0.0;
+
+	Time1 = omp_get_wtime();
+
+#pragma omp parallel for private(i, j)
+	for (i = 0; i < m_ar; i++) {
+		// k goes through the elements of the matrix A
+		for (k = 0; k < m_ar; k++) {
+			double elementA_i_k = pha[i * m_ar + k]; // element A[i,k]7
+            #pragma omp parallel for private(k)
+			for (j = 0; j < m_br; j++) {
+				// multiplies by the corresponding line in B
+				phc[i * m_br + j] += elementA_i_k * phb[k * m_br + j];
+			}
+		}
+	}
+
+	Time2 = omp_get_wtime();
+	sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
+	cout << st;
+
+	// display 10 elements of the result matrix tto verify correctness
+	cout << "Result matrix: " << endl;
+	for(i=0; i<1; i++)
+	{	for(j=0; j<min(10,m_br); j++)
+		cout << phc[j] << " ";
+	}
+	cout << endl;
+
+	free(pha);
+	free(phb);
+	free(phc);
+}
 // add code here for block x block matriz multiplication
 void OnMultBlock(int m_ar, int m_br, int bkSize)
 {
@@ -335,8 +392,10 @@ int main (int argc, char *argv[]) {
         OnMult(matrix_size, matrix_size);
     } else if (method == "line") {
         OnMultLine(matrix_size, matrix_size);
-    } else if (method == "parallel") {
-        OnMultLineParallel(matrix_size, matrix_size);
+    } else if (method == "parallel1") {
+        OnMultLineParallel1(matrix_size, matrix_size);
+    } else if (method == "parallel2") {
+        OnMultLineParallel2(matrix_size, matrix_size);
     } else if (method == "block") {
         OnMultBlock(matrix_size, matrix_size, block_size);
     } else {
